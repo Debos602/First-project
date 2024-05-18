54df3b8f4f8966e866029e402f5dda2e8b5e6dc4
unknown <debos.das.02@gmail.com>
2024-05-19 03:03:49 +0600

    create student feature

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { StudentRoutes } from './modules/student/student.route';
 const app: Application = express();
 
 // parsers
@@ -7,10 +8,17 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
+// api/v1/create-student
+
+// application routes
+app.use('/api/v1/students', StudentRoutes);
+
+const getAController = (req: Request, res: Response) => {
   const a = 10;
   res.send(a);
-});
+};
+
+app.get('/', getAController);
 
 console.log(process.cwd());
 export default app;
